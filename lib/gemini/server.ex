2e5a0b3507cb218e761a2a7869f89f7b838bc186
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2020-11-30 20:43:36 +0200

    Simplify server

--- lib/gemini/server.ex
+++ lib/gemini/server.ex
@@ -1,51 +1,59 @@
 defmodule Gemini.Server do
-  use GenServer
+  use Task, restart: :permanent
 
   require Logger
 
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, opts)
+  def start_link(arg) do
+    Task.start_link(__MODULE__, :listen, [arg])
   end
 
-  def init(args) do
-    port = Keyword.get(args, :port, 1965)
+  def listen(arg) do
+    port = Keyword.get(arg, :port, 1965)
 
-    {:ok, listen_socket} =
+    {:ok, socket} =
       :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
 
     Logger.info("Accepting connections on port #{port}")
 
-    accept_connections(listen_socket)
+    accept_connections(socket)
   end
 
   defp accept_connections(listen_socket) do
-    {:ok, socket} = :gen_tcp.accept(listen_socket)
+    case :gen_tcp.accept(listen_socket) do
+      {:ok, socket} ->
+        {:ok, pid} =
+          Task.Supervisor.start_child(
+            Gemini.RequestSupervisor,
+            fn ->
+              Logger.debug("Connection opened")
+              serve_connection(socket)
+            end
+          )
 
-    {:ok, pid} =
-      Task.Supervisor.start_child(
-        Gemini.RequestSupervisor,
-        fn -> serve(socket) end
-      )
-
-    :ok = :gen_tcp.controlling_process(socket, pid)
+        :ok = :gen_tcp.controlling_process(socket, pid)
+      {:error, error} ->
+        Logger.error(IO.inspect error)
+    end
 
     accept_connections(listen_socket)
   end
 
-  defp serve(socket) do
-    socket
-    |> read_line()
-    |> write_line(socket)
-
-    serve(socket)
-  end
-
-  defp read_line(socket) do
-    {:ok, data} = :gen_tcp.recv(socket, 0)
-
-    Logger.info data
-
-    data
+  defp serve_connection(socket) do
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, data} ->
+        Logger.info(data)
+        case data do
+          "\r\n" ->
+            Logger.debug("End of message")
+          "\n" ->
+            Logger.debug("End of message")
+          data ->
+            write_line(data, socket)
+            serve_connection(socket)
+        end
+      {:error, :closed} ->
+        Logger.debug("Connection closed")
+    end
   end
 
   defp write_line(line, socket) do
